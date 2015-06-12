7ba55ba6a6d1255eec3285f0689bd1b518044842
dukpa <jigme.dukpa@gmail.com>
2015-06-11 21:35:10 -0400

    added validation to guess

--- player.rb
+++ player.rb
@@ -4,5 +4,20 @@ class Player
 
   def get_guess
     guess = gets.chomp.split("")
+    
+    if validate(guess)
+      guess
+    else
+      puts "The guess must be a 4 digit number."
+      get_guess
+    end
+  end
+
+  private
+
+  def validate(guess)
+    guess.length == 4 &&
+      guess.uniq.length == 4 &&
+      guess.all? { |i| "0123456789".include?(i) }
   end
 end
